dd61aece475bae4f9540c42bad2a1b184d48d71e
anvaka <anvaka@gmail.com>
2023-05-07 00:50:07 -0700

    use main branch

--- demo/interactive/deploy.sh
+++ demo/interactive/deploy.sh
@@ -5,7 +5,7 @@ cd ./dist
 git init
 git add .
 git commit -m 'push to gh-pages'
-git push --force git@github.com:anvaka/isect.git master:gh-pages
+git push --force git@github.com:anvaka/isect.git main:gh-pages
 cd ../
 git tag `date "+release-%Y%m%d%H%M%S"`
 git push --tags
